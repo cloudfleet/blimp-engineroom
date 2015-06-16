f697e11e60947583ceba3f9bd247c603cb9b8fe8
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 18:29:57 +0200

    remove nginx to make sure it picks up the changes in docker-compose.yml

--- bin/start-missing-user-containers.sh
+++ bin/start-missing-user-containers.sh
@@ -7,5 +7,6 @@ $DIR/update-config-cache.sh
 (cd /opt/cloudfleet/data/config/cache && \
  docker-compose -p blimp stop pagekite && \
  docker-compose -p blimp stop nginx && \
+ docker-compose -p blimp rm -f && \
  docker-compose -p blimp up --no-recreate -d \
 )
