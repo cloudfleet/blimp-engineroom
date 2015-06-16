c20c705392778677124defb4c9013215efa17d43
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 17:41:30 +0200

    add script that starts missing user containers and restarts nginx and pagekite

---
+++ bin/start-missing-user-containers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname $0 )" && pwd )
+
+$DIR/update-config-cache.sh
+
+(cd /opt/cloudfleet/data/config/cache && \
+ docker-compose -p blimp stop pagekite \
+ docker-compose -p blimp stop nginx \
+ docker-compose -p blimp up --no-recreate -d \
+)
