c876b571aa104fea275acf9a742e7e1f6f5bfbc1
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 18:14:40 +0200

    fix start-missing-user-containers.sh

--- bin/start-missing-user-containers.sh
+++ bin/start-missing-user-containers.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname $0 )" && pwd )
 $DIR/update-config-cache.sh
 
 (cd /opt/cloudfleet/data/config/cache && \
- docker-compose -p blimp stop pagekite \
- docker-compose -p blimp stop nginx \
+ docker-compose -p blimp stop pagekite && \
+ docker-compose -p blimp stop nginx && \
  docker-compose -p blimp up --no-recreate -d \
 )
