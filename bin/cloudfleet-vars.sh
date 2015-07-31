1e59012c2e3f682f2778444965d1bacb05dde8aa
Christoph Witzany <christoph@web.crofting.com>
2015-07-31 18:05:29 +0200

    make docker registry architecture dependent

--- bin/cloudfleet-vars.sh
+++ bin/cloudfleet-vars.sh
@@ -1,3 +1,7 @@
 CLOUDFLEET_HOST=blimpyard.cloudfleet.io:443
-CLOUDFLEET_REGISTRY=registry.marina.io
 CLOUDFLEET_MAIL_RELAY=cloudfleet.cloudfleet.io
+if [ lscpu | grep ARMv7 ]; then
+  CLOUDFLEET_REGISTRY=registry.marina.io
+else
+  CLOUDFLEET_REGISTRY=registry.hub.docker.com
+fi
