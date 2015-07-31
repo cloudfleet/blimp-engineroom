5845276f8689a85f5269eec88b7997f368a77fca
Christoph Witzany <christoph@web.crofting.com>
2015-07-31 20:08:35 +0200

    make grep for arm case insensitive

--- bin/cloudfleet-vars.sh
+++ bin/cloudfleet-vars.sh
@@ -1,6 +1,6 @@
 CLOUDFLEET_HOST=blimpyard.cloudfleet.io:443
 CLOUDFLEET_MAIL_RELAY=cloudfleet.cloudfleet.io
-if lscpu | grep ARMv7 ; then
+if lscpu | grep -i ARMv7 ; then
   CLOUDFLEET_REGISTRY=registry.marina.io
 else
   CLOUDFLEET_REGISTRY=registry.hub.docker.com
