20395dc994cc5f20d8ad65dec8ab8ea07ba634f1
Christoph Witzany <christoph@web.crofting.com>
2015-07-31 17:42:56 +0200

    adapt to api with generic images

--- bin/initialize-config.sh
+++ bin/initialize-config.sh
@@ -31,35 +31,36 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 EOF
 
 
+CLOUDFLEET_OTP=$(ip addr | grep -1 eth0: | tail -1 | awk '{print $2}' | sed s/://g)
+
 mkdir -p /opt/cloudfleet/data/shared/tls
 if [ ! -f /opt/cloudfleet/data/shared/tls/tls_key.pem ]; then
-  CLOUDFLEET_DOMAIN=$(cat /opt/cloudfleet/init/domain.txt)
+  $DIR/request_domain.py $CLOUDFLEET_OTP && \
   openssl req -x509 -nodes -newkey rsa:4096 \
     -keyout /opt/cloudfleet/data/shared/tls/tls_key.pem \
     -out /opt/cloudfleet/data/shared/tls/tls_req.pem \
-    -subj /C=/ST=/L=/O=CloudFleet/OU=/CN=blimp.$CLOUDFLEET_DOMAIN
-
-    cp /opt/cloudfleet/data/shared/tls/tls_req.pem /opt/cloudfleet/data/shared/tls/tls_crt.pem
+    -subj /C=/ST=/L=/O=CloudFleet/OU=/CN=blimp.$(cat /opt/cloudfleet/data/config/domain.txt) && \
+  cp /opt/cloudfleet/data/shared/tls/tls_req.pem /opt/cloudfleet/data/shared/tls/tls_crt.pem
 fi
 
-if [ ! -f /opt/cloudfleet/data/shared/tls/cert-requested.status ]; then
-  CLOUDFLEET_OTP=$(cat /opt/cloudfleet/init/otp.txt)
-  CLOUDFLEET_DOMAIN=$(cat /opt/cloudfleet/init/domain.txt)
-  $DIR/request_cert.py \
-      $CLOUDFLEET_DOMAIN \
-      $CLOUDFLEET_OTP \
-      /opt/cloudfleet/data/shared/tls/tls_req.pem \
-      && \
-      touch /opt/cloudfleet/data/shared/tls/cert-requested.status
-fi
+if [ -f /opt/cloudfleet/data/config/domain.txt ]; then
+  CLOUDFLEET_DOMAIN=$(cat /opt/cloudfleet/data/config/domain.txt)
+  #if [ ! -f /opt/cloudfleet/data/shared/tls/cert-requested.status ]; then
+    $DIR/request_cert.py \
+        $CLOUDFLEET_DOMAIN \
+        $CLOUDFLEET_OTP \
+        /opt/cloudfleet/data/shared/tls/tls_req.pem \
+        && \
+        touch /opt/cloudfleet/data/shared/tls/cert-requested.status
+  #fi
 
-if [ ! -f /opt/cloudfleet/data/config/blimp-vars.sh ]; then
-  $DIR/request_secret.py \
-      $CLOUDFLEET_DOMAIN \
-      /opt/cloudfleet/data/shared/tls/tls_key.pem \
-      /opt/cloudfleet/data/config/blimp-vars.sh
+  if [ ! -f /opt/cloudfleet/data/config/blimp-vars.sh ]; then
+    $DIR/request_secret.py \
+        $CLOUDFLEET_DOMAIN \
+        /opt/cloudfleet/data/shared/tls/tls_key.pem \
+        /opt/cloudfleet/data/config/blimp-vars.sh
+  fi
 fi
-
 
 echo "====================================="
 echo "`date "+%F %T"`  Initialized config where necessary ... "
---
+++ bin/request_domain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""An example demonstrating the client-side usage
+of the cretificate request API endpoint.
+"""
+
+import requests, sys, json
+
+otp = sys.argv[1]
+
+domain_req_url = 'https://spire.cloudfleet.io/api/v1/domain'
+domain_txt_path = '/opt/cloudfleet/data/config/domain.txt'
+
+print('retrieving domain for blimp: ' + domain_req_url)
+
+r = requests.get(domain_req_url, headers={'X_AUTH_OTP': otp})
+
+if r.status_code == 200:
+    print('Success: %s' % r.text)
+    result_dict = r.json()
+
+    if "domain" in result_dict:
+        with open(domain_txt_path, 'w') as domain_txt_file:
+            domain_txt_file.write(result_dict['domain'])
+        quit(0)
+    else:
+        quit(1)
+else:
+    print('Error: %s' % r.text)
+    quit(1)
