9fe6b642facd0c9964c171fc474514dd64b00e19
Dražen Lučanin <kermit666@gmail.com>
2015-10-13 23:42:20 +0200

    wipe_disks.sh also resets /etc/fstab

--- bin/cryptpart/wipe_disks.sh
+++ bin/cryptpart/wipe_disks.sh
@@ -7,9 +7,10 @@ DIR=$( cd "$( dirname $0 )" && pwd )
 
 $DIR/close_partitions.sh
 
-hdd="/dev/sda /dev/sdb"
-for i in $hdd; do
-    echo "d
+function wipe_drives(){
+    hdd="/dev/sda /dev/sdb"
+    for i in $hdd; do
+	echo "d
 1
 d
 2
@@ -23,5 +24,22 @@ p
 w
 " | fdisk $i; done
 
-mkfs.ext3 /dev/sda1 -L ${STORAGE_PARTITION_LABEL}
-mkfs.ext3 /dev/sdb1 -L ${KEY_PARTITION_LABEL}
+    mkfs.ext3 /dev/sda1 -L ${STORAGE_PARTITION_LABEL}
+    mkfs.ext3 /dev/sdb1 -L ${KEY_PARTITION_LABEL}
+}
+
+#wipe_drives
+
+# revert fstab, crypttab
+
+if [ -f /etc/fstab.original ]
+then
+    # we already made a copy of the first fstab, bring it back
+    mv /etc/fstab /etc/fstab.backup
+    cp /etc/fstab.original /etc/fstab    
+else
+    # zZZ
+    :
+fi
+
+mv /etc/crypttab /etc/crypttab.backup
