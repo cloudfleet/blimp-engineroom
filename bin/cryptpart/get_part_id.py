a93a13af36ca9895ab047dbcd560712547c8bda1
Dražen Lučanin <kermit666@gmail.com>
2015-11-09 17:28:23 +0100

    get_part_id get real info

--- bin/cryptpart/get_part_id.py
+++ bin/cryptpart/get_part_id.py
@@ -7,20 +7,22 @@
 """
 
 import sys
+import subprocess
 
-# TODO: get output of:
-# ls -l /dev/disk/by-id/* | grep sda1
+# TODO: rewrite using os.listdir
 
 # e.g.
 
-output = """
-lrwxrwxrwx 1 root root 10 Oct 12 16:54 /dev/disk/by-id/ata-WDC_WD2500BEVT-22ZCT0_WD-WXE908VF0470-partX -> ../../sdaX
-lrwxrwxrwx 1 root root 10 Oct 12 16:54 /dev/disk/by-id/wwn-0x60015ee0000b237f-partX -> ../../sdaX
-"""
+# output = """
+# lrwxrwxrwx 1 root root 10 Oct 12 16:54 /dev/disk/by-id/ata-WDC_WD2500BEVT-22ZCT0_WD-WXE908VF0470-partX -> ../../sdaX
+# lrwxrwxrwx 1 root root 10 Oct 12 16:54 /dev/disk/by-id/wwn-0x60015ee0000b237f-partX -> ../../sdaX
+# """
 
 def main():
     name = sys.argv[1].split('/')[2]
+    output = subprocess.check_output('/bin/ls -l /dev/disk/by-id/*', shell=True)
     for line in output.split('\n'):
+        print(line)
         if len(line.rstrip()) > 0:
             without_date = line.split('/dev/disk/by-id/')[1]
             partid, partname = without_date.split(' -> ../../')
