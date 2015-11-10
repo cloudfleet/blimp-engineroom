70a7f15100e2942bc26f0377e4f678111cbf6c21
Dražen Lučanin <kermit666@gmail.com>
2015-11-10 17:46:22 +0100

    keyscript for auto-mounting usb

---
+++ bin/cryptpart/keyscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# keyscript called from /etc/crypttab to mount the key USB and echo the key
+# usage:
+# ./keyscript.sh <key_partition>
+
+set -e
+
+# not setting these using ./set_partition_vars.sh to reduce the number of dependencies
+# (as this script is called early in the boot process)
+KEY_PARTITION=$1
+KEY_MOUNTPOINT=/mnt/storage-key
+
+if mountpoint -q "$KEY_MOUNTPOINT"; then
+    : # already mounted, do nothing
+else
+    mount -t auto $KEY_PARTITION $KEY_MOUNTPOINT
+fi
+
+cat /mnt/storage-key/key
