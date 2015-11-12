1636be77e4864122262d9fd60d743edcc3d35996
Dražen Lučanin <kermit666@gmail.com>
2015-11-12 20:29:04 +0100

    update crontab to decrypt partitions if present

--- bin/create-crontab.sh
+++ bin/create-crontab.sh
@@ -8,6 +8,7 @@
 crontab - <<EOF
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 0 4 * * * sleep ${RANDOM:0:2}m; /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
-@reboot /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
+@reboot /opt/cloudfleet/engineroom/bin/startup.sh
 EOF
+#@reboot /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
 
