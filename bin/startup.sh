0bdd835b75d585164a9ed08909cd989190bd50d0
Dražen Lučanin <kermit666@gmail.com>
2015-11-20 16:07:06 +0000

    log cryptpart_startup to a location which exists

--- bin/startup.sh
+++ bin/startup.sh
@@ -6,9 +6,11 @@ DIR=$( cd "$( dirname $0 )" && pwd )
 
 # open encrypted partitions
 # (can't happen sooner due to systemd ignoring keyscript on Debian >=8.1)
-$DIR/cryptpart/cryptpart_startup.sh
+$DIR/cryptpart/cryptpart_startup.sh  >> /opt/cloudfleet/scyprpart-startup.log 2>&1
+
 
 # now start the usual engineroom drill
-/opt/cloudfleet/engineroom/bin/upgrade-blimp.sh 
+mkdir -p /opt/cloudfleet/data/logs/
+/opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
 
 exit
