198abea8f20ba86f01904ee91c95a34bbaf3c39b
Dražen Lučanin <kermit666@gmail.com>
2015-11-20 17:38:25 +0000

    crontab - startup.sh logs to existing location for now

--- bin/startup.sh
+++ bin/startup.sh
@@ -6,11 +6,11 @@ DIR=$( cd "$( dirname $0 )" && pwd )
 
 # open encrypted partitions
 # (can't happen sooner due to systemd ignoring keyscript on Debian >=8.1)
-$DIR/cryptpart/cryptpart_startup.sh  >> /opt/cloudfleet/scyprpart-startup.log 2>&1
+$DIR/cryptpart/cryptpart_startup.sh
 
 
 # now start the usual engineroom drill
-mkdir -p /opt/cloudfleet/data/logs/
-/opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
+#mkdir -p /opt/cloudfleet/data/logs/
+/opt/cloudfleet/engineroom/bin/upgrade-blimp.sh
 
 exit
