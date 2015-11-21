6c60f7c940d36c578d03b84bc9963fe7fb02579a
Mark <evenson@panix.com>
2015-11-21 17:50:42 +0100

    Add diagnostics

--- bin/init.bash
+++ bin/init.bash
@@ -4,4 +4,9 @@ export CF_VAR=${CF}/var
 mkdir -p ${CF_VAR} || echo "Failed to create ${CF_VAR}. Continuing."
 
 # Debug settings
-set -x 
+set -x
+
+# Emit diagnostics
+date 
+uname -a 
+lsb_release -a 
