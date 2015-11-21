e047d7e8e4f889297ecd661c89170e4bd65bc0c6
Mark <evenson@panix.com>
2015-11-21 16:59:14 +0100

    Introduce globals describing commonly used path location
    CF_VAR references a non-encrypted, non-persistent between session
    storage.  We place transitory logs here.

--- bin/create-crontab.sh
+++ bin/create-crontab.sh
@@ -8,7 +8,7 @@
 crontab - <<EOF
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 0 4 * * * sleep ${RANDOM:0:2}m; /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
-@reboot /opt/cloudfleet/engineroom/bin/startup.sh >> /opt/cloudfleet/startup.log 2>&1
+@reboot /opt/cloudfleet/engineroom/bin/startup.sh >> /opt/cloudfleet/var/startup.log 2>&1
 EOF
 #@reboot /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
 
--- bin/init.bash
+++ bin/init.bash
@@ -1,2 +1,7 @@
+# Define and ensure directories exist
+export CF=/opt/cloudfleet
+export CF_VAR=${CF_ROOT}/var
+mkdir -p ${CF_VAR}
+
 # Debug settings
 set -x 
