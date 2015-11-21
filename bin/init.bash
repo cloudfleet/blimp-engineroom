a5612f7a0c3a682377a58536b5fb08a3216442f5
Mark <evenson@panix.com>
2015-11-21 17:11:39 +0100

    Note failure to ensure existence of /var/cloudfleet/var

--- bin/init.bash
+++ bin/init.bash
@@ -1,7 +1,7 @@
 # Define and ensure directories exist
 export CF=/opt/cloudfleet
 export CF_VAR=${CF}/var
-mkdir -p ${CF_VAR}
+mkdir -p ${CF_VAR} || echo "Failed to create ${CF_VAR}. Continuing."
 
 # Debug settings
 set -x 
