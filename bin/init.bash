cd5c599a199018adcecc38629bd019fbe546dd9c
Mark <evenson@panix.com>
2015-11-21 18:34:13 +0100

    Guard against whitespace in expansions

--- bin/init.bash
+++ bin/init.bash
@@ -1,8 +1,8 @@
-if [ ! -z $CF_INITIALIZED_P ] ; then
+if [ ! -z "$CF_INITIALIZED_P" ] ; then
     # Define and ensure directories exist
     export CF=/opt/cloudfleet
     export CF_VAR=${CF}/var
-    mkdir -p ${CF_VAR} || echo "Failed to create ${CF_VAR}. Continuing."
+    mkdir -p "${CF_VAR}" || echo "Failed to create ${CF_VAR}. Continuing."
 
     # Debug settings
 
