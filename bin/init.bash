0c8a2e3aa0b2a6000dd4e793ab649289ac43aca1
Mark <evenson@panix.com>
2015-11-21 18:33:15 +0100

    Guard repeated invocations of initialization

--- bin/init.bash
+++ bin/init.bash
@@ -1,12 +1,22 @@
-# Define and ensure directories exist
-export CF=/opt/cloudfleet
-export CF_VAR=${CF}/var
-mkdir -p ${CF_VAR} || echo "Failed to create ${CF_VAR}. Continuing."
+if [ ! -z $CF_INITIALIZED_P ] ; then
+    # Define and ensure directories exist
+    export CF=/opt/cloudfleet
+    export CF_VAR=${CF}/var
+    mkdir -p ${CF_VAR} || echo "Failed to create ${CF_VAR}. Continuing."
 
-# Debug settings
-set -x
+    # Debug settings
 
-# Emit diagnostics
-date 
-uname -a 
-lsb_release -a 
+    ## Emit diagnostics
+    date 
+    uname -a 
+    lsb_release -a
+    lsbls --fs
+
+    ## Log all command invocations
+    set -x
+
+fi
+
+export CF_INITIALIZED_P=true
+
+
