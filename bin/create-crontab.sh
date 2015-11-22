18348e5026a82f9ddda84b5c19acc150961aa4eb
Mark <evenson@panix.com>
2015-11-22 16:27:30 +0100

    Use version control

--- bin/create-crontab.sh
+++ bin/create-crontab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Setup crontab
 #
-# XXX This obliterates any existing crontab entries which is probably
-# not a good long-term strategy.
+# XXX This obliterates any existing crontab entries everytime
+#
 #
 # TODO: centralize setting values for directories (location of engineroom, logs, etc.)
 crontab - <<EOF
@@ -10,5 +10,4 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 0 4 * * * sleep ${RANDOM:0:2}m; /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
 @reboot /opt/cloudfleet/engineroom/bin/startup.sh >> /opt/cloudfleet/var/startup.log 2>&1
 EOF
-#@reboot /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/blimp-upgrade.log 2>&1
 
