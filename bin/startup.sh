01a5d37479946d4f55f40da4d9cdeb429bdfa649
Mark <evenson@panix.com>
2015-11-22 16:26:34 +0100

    Guard against expansion in main startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
-
-# engineroom startup script meant to run on every reboot
+# blimp-engineroom startup script meant to run on every reboot
 
 DIR=$( cd "$( dirname $0 )" && pwd )
 . "${DIR}/init.bash"
 
 # open encrypted partitions
 # (can't happen sooner due to systemd ignoring keyscript on Debian >=8.1)
-. $DIR/cryptpart/cryptpart_startup.sh
+. "$DIR/cryptpart/cryptpart_startup.sh"
 
 
 # now start the usual engineroom drill
 #mkdir -p /opt/cloudfleet/data/logs/
-. /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh
+. "$DIR/upgrade-blimp.sh"
 
 exit
