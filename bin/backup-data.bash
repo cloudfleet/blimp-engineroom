26bf4f310decff663f3d844bc9352d366cc47031
Mark <easye@gmail.com>
2015-11-23 11:41:48 +0100

    Debugged version of backup script

--- bin/backup-data.bash
+++ bin/backup-data.bash
@@ -5,12 +5,18 @@
 DIR=$( cd "$( dirname $0 )" && pwd )
 . "${DIR}/init.bash"
 
+# as root...
+if [ $(id -u) != 0 ]; then
+    echo Insufficient privilege to backup $CF_DATA && exit 1
+fi
+
+
 backup_dir=/var/tmp
 file=opt-cloudfleet-data
 date=$( date "+%Y%m%d%H%M" )
 
-file_path="${backup_dir}/${file}-${date}"
+file_path="${backup_dir}/${file}-${date}.tar.gz"
 echo "Backing up "$CF_DATA" to $file_path"
-cd $(dirname $CF_DATA) && tar -xvz --file "${file_path} --exclude .snapshot ./data
+cd $(dirname $CF_DATA) && tar -cvz --file "${file_path}" --exclude .snapshot ./data
 
 
