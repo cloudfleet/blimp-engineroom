dbef4281a3a6953e3a3ceff7fe507c261022ad42
Mark <evenson.not.org@gmail.com>
2015-12-15 12:47:32 +0000

    check_partitions.sh: lsblk --pairs output examples

--- test/test-check-partitions.bash
+++ test/test-check-partitions.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# lsblk-blimpie-20151208a currently chokes the scrupt
-for file in lsblk-blimpie-20151208a lsblk-blimpie-20151208b; do 
+# Old 'lsblk' input examples
+#inputs="lsblk-blimpie-20151208a lsblk-blimpie-20151208b"
+# New 'lsblk --pairs' input examples
+inputs="lsblk-blimpie-pairs-20151215a"
+
+for file in $inputs; do 
     cat $file | ../bin/cryptpart/check_partitions.py
 done
 
