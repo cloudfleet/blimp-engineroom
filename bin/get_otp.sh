bf8bdde1a4d1908e2631f5465be6f5bc354a4227
Christoph Witzany <christoph@web.crofting.com>
2018-01-31 16:13:18 +0100

    Remove certificate handling from engineroom

--- bin/get_otp.sh
+++ bin/get_otp.sh
@@ -4,7 +4,11 @@
 #  - OTP is used for initially talking to the CloudFleet Spire service
 #  - right now, the device's mac address is used
 
-CLOUDFLEET_OTP=$(ip addr | grep -1 eth0: | tail -1 | awk '{print $2}' | sed s/://g)
+if [ -f /etc/cloudfleet/otp.txt ]; then
+  CLOUDFLEET_OTP=$(cat /etc/cloudfleet/otp.txt)
+else
+  CLOUDFLEET_OTP=$(ip addr | grep -1 eth0: | tail -1 | awk '{print $2}' | sed s/://g)
+fi
 
 echo $CLOUDFLEET_OTP
 
